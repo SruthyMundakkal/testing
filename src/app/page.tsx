d1b28ef6fd5b07c0f06e587a73b721fc9d59212d
SruthyMundakkal <sruthy228.1997@gmail.com>
2024-10-09 12:23:05 -0300

    changes done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,27 @@
 
 import Banner from "@/components/Banner";
 import OrdersReport from "@/components/OrdersReport";
+import { useState } from "react";
+import LoadingOverlay from "@/components/LoadingOverlay";
 
 export default function Home() {
+
+  //-----------------------------event handlers
+  const showOrders = (e:any) => setAppState(2);
+
+
+  //------------------------------State variables
+  // 1 - no orders
+  // 2 - orders loading
+  // 3 - orders have been loaded
+
+  const [appState, setAppState] = useState(1);
 
   // ---------------------------- rendering to DOM
   return (
     <main className="grid grid-rows-1 grid-cols-1 gap-0 text-content">
+      <LoadingOverlay show={(appState == 2) ? true : false} bgColor="#b82308"></LoadingOverlay>
+
 
       <Banner />
 
@@ -19,7 +34,9 @@ export default function Home() {
           <div className="text-accent text-3xl font-bold mb-2.5">Welcome loyal pizza dispatcher....</div>Click the &quot;Get Orders&quot; button below to view all current orders that need to be delivered.
           <div>
               <button 
-                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5">Get Orders</button>
+                className="bg-accent border-none rounded-md p-2.5 text-white hover:bg-greyContent mt-5" onClick={showOrders}>
+                  Get Orders
+                  </button>
           </div>
         </div>
         <div className="shrink-0 text-lg text-right text-greyContent hidden md:block">
@@ -34,7 +51,7 @@ export default function Home() {
 
         <div id="output" className="divide-dashed divide-y-2 divide-accent">
 
-          <OrdersReport />
+          <OrdersReport setAppState = {setAppState} appState={appState} />
 
         </div>
       </div>
--- src/components/OrdersReport.tsx
+++ src/components/OrdersReport.tsx
@@ -1,12 +1,48 @@
 "use client";
 
+import { useEffect, useState } from 'react';
+
 import { getJSONData } from '@/tools/Toolkit';
 import { Orders, Order } from '@/tools/orders.model';
 
-export default function OrdersReport() {
+export default function OrdersReport({setAppState, appState}: {setAppState:Function, appState:number}) {
     // retrieve server sided script
     const RETRIEVE_SCRIPT:string = "https://www.seanmorrow.ca/_lessons/retrieveOrder.php";
   
+    //----------------------------------------private methods
+    const getOrders = async () => {
+        const data: Orders = await getJSONData(RETRIEVE_SCRIPT, false, true);
+        console.log(data);
 
-    return (<>No orders retrieved...</>);
+        // save it in a state variable - because it is used in JSX and needs to persist
+        setOrders(data.orders);
+
+        // data all loaded! change app state of web app
+        setAppState(3);
+
+    }
+
+    // ---------------------------------------use effects
+    useEffect(() => {
+        if (appState == 2) getOrders();
+    }, [appState]);
+
+    //----------------------------------------state variables
+    const [orders, setOrders] = useState<Order[]>([]);
+
+    if (appState == 1) {
+        return (<>No orders retrieved...</>);
+    } else if ( appState == 3) {
+        return (
+            <>
+                !!! render out orders content here!!
+            </>
+        )
+    }
+
+    
 }
+
+
+
+
