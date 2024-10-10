979e8060d6dc2b6d19ab991d58ce59e534261268
SruthyMundakkal <sruthy228.1997@gmail.com>
2024-10-10 10:10:22 -0300

    challenge 2

--- src/components/OrdersReport.tsx
+++ src/components/OrdersReport.tsx
@@ -62,9 +62,9 @@ export default function OrdersReport({setAppState, appState}: {setAppState:Funct
                         <div><i className="fas fa-pizza-slice"></i><strong> Pizza Size:</strong></div>
                         <div>{order.size}</div>
                         <div><strong>Order Details:</strong></div>
-                        <div>{order.toppings.map(item => item.topping).join(", ")}</div>
+                        <div>{order.toppings.map(item =>(<p>{item.topping}</p>) )}</div>
                         <div><strong>Order Notes:</strong> </div>
-                        <div>{order.notes.map(item => item.note).join(", ")}</div>
+                        <div>{order.notes.map(item => (<p>{item.note}</p>) )}</div>
                     </div>
                     ))}
                     {/* <div className="id"></div>
